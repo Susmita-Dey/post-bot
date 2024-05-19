c1549cbb11840d7d012fa5d5a1790ceafbcea380
Gary Hilares <dev.garyhilares@gmail.com>
2024-05-18 23:08:32 -0500

    Applied formatter

--- bot.py
+++ bot.py
@@ -10,10 +10,12 @@ from webserver import keep_alive  # Import the keep_alive function
 load_dotenv()
 
 # Use the environment variables
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-ALLOWED_ROLE_ID_str = os.getenv('ALLOWED_ROLE_ID')
-JOB_CHANNEL_ID_str = os.getenv('JOB_CHANNEL_ID')  # Channel ID for job postings
-SERVICE_CHANNEL_ID_str = os.getenv('SERVICE_CHANNEL_ID')  # Channel ID for service postings
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+ALLOWED_ROLE_ID_str = os.getenv("ALLOWED_ROLE_ID")
+JOB_CHANNEL_ID_str = os.getenv("JOB_CHANNEL_ID")  # Channel ID for job postings
+SERVICE_CHANNEL_ID_str = os.getenv(
+    "SERVICE_CHANNEL_ID"
+)  # Channel ID for service postings
 
 # Convert environment variables to integers
 ALLOWED_ROLE_ID = int(ALLOWED_ROLE_ID_str) if ALLOWED_ROLE_ID_str else None
@@ -23,28 +25,33 @@ SERVICE_CHANNEL_ID = int(SERVICE_CHANNEL_ID_str) if SERVICE_CHANNEL_ID_str else 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'Bot is ready. Logged in as {bot.user}')
+    print(f"Bot is ready. Logged in as {bot.user}")
 
-@bot.command(name='postjob')
+
+@bot.command(name="postjob")
 async def post_job(ctx, *, message: str):
     await post_to_channel(ctx, message, JOB_CHANNEL_ID, "New Job Posting")
 
-@bot.command(name='postservices')
+
+@bot.command(name="postservices")
 async def post_services(ctx, *, message: str):
     await post_to_channel(ctx, message, SERVICE_CHANNEL_ID, "New Service Offering")
 
-@bot.command(name='post')
+
+@bot.command(name="post")
 async def post(ctx, channel: discord.TextChannel, title: str, *, message: str):
     await post_to_any_channel(ctx, channel.id, title, message)
+
 
 async def post_to_channel(ctx, message, channel_id, title):
     try:
         if channel_id is None:
-            await ctx.send('Target channel not found.')
+            await ctx.send("Target channel not found.")
             return
 
         # Check if the author has the allowed role ID
@@ -55,20 +62,21 @@ async def post_to_channel(ctx, message, channel_id, title):
                 embed = discord.Embed(
                     title=title,
                     description=message,
-                    color=0xffd700  # Gold color for the embed border
+                    color=0xFFD700,  # Gold color for the embed border
                 )
                 # Add a footer to the embed
-                embed.set_footer(text='- MoneyMotives Team')
+                embed.set_footer(text="- MoneyMotives Team")
                 await channel.send(embed=embed)
                 await ctx.message.delete()  # Delete the original command message
             else:
-                await ctx.send('Target channel not found.')
+                await ctx.send("Target channel not found.")
         else:
-            await ctx.send('You do not have the required role to use this command.')
+            await ctx.send("You do not have the required role to use this command.")
     except Exception as e:
         print(f"An error occurred: {e}")
         traceback.print_exc()
-        await ctx.send('An error occurred while trying to execute the command.')
+        await ctx.send("An error occurred while trying to execute the command.")
+
 
 async def post_to_any_channel(ctx, channel_id, title, message):
     try:
@@ -80,20 +88,21 @@ async def post_to_any_channel(ctx, channel_id, title, message):
                 embed = discord.Embed(
                     title=title,
                     description=message,
-                    color=0xffd700  # Gold color for the embed border
+                    color=0xFFD700,  # Gold color for the embed border
                 )
                 # Add a footer to the embed
-                embed.set_footer(text='- MoneyMotives Team')
+                embed.set_footer(text="- MoneyMotives Team")
                 await channel.send(embed=embed)
                 await ctx.message.delete()  # Delete the original command message
             else:
-                await ctx.send('Target channel not found.')
+                await ctx.send("Target channel not found.")
         else:
-            await ctx.send('You do not have the required role to use this command.')
+            await ctx.send("You do not have the required role to use this command.")
     except Exception as e:
         print(f"An error occurred: {e}")
         traceback.print_exc()
-        await ctx.send('An error occurred while trying to execute the command.')
+        await ctx.send("An error occurred while trying to execute the command.")
+
 
 # Keep the web server alive
 keep_alive()
